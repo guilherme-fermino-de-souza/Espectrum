bf168d12b68d0394ff62dad85a7a3158058eeade
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-06-20 12:27:50 -0300

    Update .sql

--- Arquivos/.sql
+++ Arquivos/.sql
@@ -1,7 +1,6 @@
-CREATE DATABASE bdIntea
-use bdIntea
---DROP Database bdIntea
-  
+CREATE DATABASE bdRedeSocialAutistas
+use bdRedeSocialAutistas
+--DROP Database bdRedeSocialAutistas
 CREATE TABLE tbAutista(
 idAutista INT PRIMARY KEY IDENTITY(1,1),
 emailAutista VARCHAR(100),
@@ -9,14 +8,14 @@ senhaAutista VARCHAR(50),
 cipteia VARCHAR(50),
 generoAustista varchar(20),
 dataNascAutista DATE,
-cep INT,
-logradouro VARCHAR(50),
-endereco VARCHAR(50),
-rua varchar(50),
-bairro VARCHAR(100),
-numero INT,
-cidade varchar(100),
-estado VARCHAR(100),
+cepAutista INT,
+logradouroAutista VARCHAR(50),
+enderecoAutista VARCHAR(50),
+ruaAutista varchar(50),
+bairroAutista VARCHAR(100),
+numeroAutista INT,
+cidadeAutista varchar(100),
+estadoAutista VARCHAR(100),
 )
 CREATE TABLE tbFoneAutista(
 idFoneAustista INT PRIMARY KEY IDENTITY(1,1),
@@ -31,20 +30,22 @@ emailPsicologo VARCHAR(100),
 senhaPsicologo VARCHAR(100),
 crmPsicologo varchar(50),
 dataNascPsicologo DATE,
-cep INT,
-logradouro VARCHAR(50),
-endereco VARCHAR(50),
-rua varchar(50),
-bairro VARCHAR(100),
-numero INT,
-cidade varchar(100),
-estado VARCHAR(100),
+cepPsicologo INT,
+logradouroPsicologo VARCHAR(50),
+enderecoPsicologo VARCHAR(50),
+ruaPsicologo varchar(50),
+bairroPsicologo VARCHAR(100),
+numeroPsicologo INT,
+cidadePsicologo varchar(100),
+estadoPsicologo VARCHAR(100),
 ) 
+  
 CREATE TABLE tbFonePsicologo(
 idFonePsicologo INT PRIMARY KEY IDENTITY(1,1),
 numero INT,
 idPsicologo INT FOREIGN KEY REFERENCES tbPsicologo(idPsicologo),
 )	
+  
 CREATE TABLE tbResponsavel(
 idResponsavel INT PRIMARY KEY IDENTITY(1,1),
 nomeResponsavel VARCHAR(50),
@@ -52,45 +53,51 @@ emailResponsavel VARCHAR(50),
 senhaResponsavel VARCHAR(100),
 dataNascimentoResponsavel DATE,
 idAutista INT FOREIGN KEY REFERENCES tbAutista(idAutista),
-cep INT,
-logradouro VARCHAR(50),
-endereco VARCHAR(50),
-rua varchar(50),
-bairro VARCHAR(100),
-numero INT,
-cidade varchar(100),
-estado VARCHAR(100),
+cepResponsavel INT,
+logradouroResponsavel VARCHAR(50),
+enderecoResponsavel VARCHAR(50),
+ruaResponsavel varchar(50),
+bairroResponsavel VARCHAR(100),
+numeroResponsavel INT,
+cidadeResponsavel varchar(100),
+estadoResponsavel VARCHAR(100),
 )
+  
 CREATE TABLE tbFoneResponsavel(
 idFoneResponsavel INT PRIMARY KEY  IDENTITY (1,1),
 numero INT,
 idResponsavel INT FOREIGN KEY REFERENCES tbResponsavel(idResponsavel)
 )
+  
 CREATE TABLE tbPostagem(
 idPostagem INT PRIMARY KEY  IDENTITY(1,1),
-idAutista INT FOREIGN KEY REFERENCES tbAutista(idAutista),
-conteudo VARCHAR(1000),
-dataPostagem DATETIME DEFAULT CURRENT_TIMESTAMP
+conteudoPostagem VARCHAR(1000),
+comentarioPostagem varchar(500),
+dataPostagem DATETIME DEFAULT CURRENT_TIMESTAMP,
+imagem
+  Postagem VARCHAR(100)
 )
-CREATE TABLE tbComentario(
+CREATE TABLE tbComentarios(
 idComentario INT PRIMARY KEY IDENTITY (1,1),
 idPostagem INT FOREIGN KEY REFERENCES tbPostagem(idPostagem),
-idAutista INT FOREIGN KEY REFERENCES tbAutista (idAutista),
+conteudoComentario VARCHAR(500)
+
+)
+CREATE TABLE tbImagems(
+idImagem INT PRIMARY KEY IDENTITY(1,1),
+imagem VARCHAR(500),
+idPostagem INT FOREIGN KEY REFERENCES tbPostagem(idPostagem),
+
 )
 
-CREATE TABLE tbSalaVirtual(
-idSalaVirtual INT PRIMARY KEY IDENTITY(1,1),
-link VARCHAR(100),
-dataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP
-)
+
 CREATE TABLE  tbConsultas(
 idConsultas INT PRIMARY KEY IDENTITY(1,1),
 idPsicologo INT FOREIGN KEY  REFERENCES tbPsicologo(idPsicologo),
 idAutista INT FOREIGN KEY REFERENCES tbAutista (idAutista),
-idSalaVirtual INT FOREIGN KEY REFERENCES tbSalaVirtual(idSalaVirtual),
 dataAgendamento DATETIME,
 DataRealizacao DATETIME,
-statusConsultas VARCHAR(50) DEFAULT'Agendada',
+statusConsultas VARCHAR(50) ,
 observacoesConsulta  VARCHAR(500),
 )
 
@@ -106,18 +113,28 @@ cpfComunidade VARCHAR(18),
 tipoComunidade VARCHAR(50),
 dataNascComunidade DATE,
 emailComunidade Varchar(500),
-cep INT,
-logradouro VARCHAR(50),
-endereco VARCHAR(50),
-rua varchar(50),
-bairro VARCHAR(100),
-numero INT,
-cidade varchar(100),
-estado VARCHAR(100),
+cepComunidade INT,
+logradouroComunidade VARCHAR(50),
+enderecoComunidade VARCHAR(50),
+ruaComunidade varchar(50),
+bairroComunidade VARCHAR(100),
+numeroComunidade INT,
+cidadeComunidade varchar(100),
+estadoComunidade VARCHAR(100),
 )
 CREATE TABLE tbAdmin(
 idAdmin INT PRIMARY KEY IDENTITY(1,1),
 emailAdmin varchar(500),
 senhaAdmin VARCHAR(500),
 cpfAdmin VARCHAR(20)
+)
+CREATE TABLE tbGruposDaComunidadeAutista(
+idGruposDaComunidadeAutista Int PRIMARY KEY IDENTITY(1,1),
+nomeGrupo VARCHAR(500),
+descricaoGrupo varchar(500),
+)
+CREATE TABLE tbGruposComunidade(
+idGruposComunidade INT PRIMARY KEY IDENTITY(1,1),
+idGruposDaComunidadeAutista INT FOREIGN KEY REFERENCES tbGruposDaComunidadeAutista(idGruposDaComunidadeAutista),
+idComunidade INT FOREIGN KEY REFERENCES tbComunidadeAutista(idComunidade)
 )
