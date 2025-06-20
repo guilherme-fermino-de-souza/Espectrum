d5ac397a9915c6a3e59cbad637fab310550f02b3
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-06-20 00:10:28 -0300

    Update my.sql

--- Arquivos/my.sql
+++ Arquivos/my.sql
@@ -6,10 +6,11 @@ CREATE TABLE tbUsuario (
   idUsuario INT AUTO_INCREMENT PRIMARY KEY,
   nomeUsuario VARCHAR(255) NOT NULL,
   emailUsuario VARCHAR(255) UNIQUE NOT NULL,
-  senhaUsuario VARCHAR(255) NOT NULL,
-  generoUsuario VARCHAR(20),
-  dataNascUsuario DATE,
-  cepUsuario CHAR(8),
+  senhaUsuario VARCHAR(255) NOT NULL, 
+  cpf VARCHAR(14) NOT NULL,
+  generoUsuario VARCHAR(20) NOT NULL,
+  dataNascUsuario DATE NOT NULL,
+  cepUsuario CHAR(8) NOT NULL,
   logradouroUsuario VARCHAR(255),
   enderecoUsuario VARCHAR(255),
   ruaUsuario VARCHAR(255),
@@ -19,13 +20,64 @@ CREATE TABLE tbUsuario (
   estadoUsuario VARCHAR(100),
   tipo_usuario ENUM('admin', 'alistico', 'autista', 'cuidador', 'profissional') NOT NULL,
 ) 
+-- telefones usuário
+CREATE TABLE tbFoneUsuario (
+  idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT UNIQUE NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES tbUsuario(idUsuario) ON DELETE CASCADE
+)
+-- alístico (mais simples de todos)
+CREATE TABLE tbAlistico(
+  idAlistico INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT UNIQUE NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES tbUsuario(idUsuario) ON DELETE CASCADE
+)
 -- autista
 CREATE TABLE tbAutista (
   idAutista INT AUTO_INCREMENT PRIMARY KEY,
   usuario_id INT UNIQUE NOT NULL,
-  cpf VARCHAR(14) NOT NULL,
-  cpteia VARCHAR(50) NOT NULL,
-  -- Outros campos específicos do autista (se quiser)
+  cpteia VARCHAR(255) NOT NULL,
   FOREIGN KEY (usuario_id) REFERENCES tbUsuario(idUsuario) ON DELETE CASCADE
 )
--- 
+-- cuidador
+CREATE TABLE tbCuidador (
+  idCuidador INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT UNIQUE NOT NULL,
+  documentoCuidador VARCHAR(255) NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES tbUsuario(idUsuario) ON DELETE CASCADE
+)
+-- este seria o antigo psicólogo
+CREATE TABLE tbProfissional (
+  idProfissional INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT UNIQUE NOT NULL,
+  areaAtuacaoProfissional VARCHAR(255) NOT NULL,
+  documentoProfissional VARCHAR(255) NOT NULL,  -- algo que comprove a funcão, talvez o crm
+  FOREIGN KEY (usuario_id) REFERENCES tbUsuario(idUsuario) ON DELETE CASCADE
+)
+-- ligação responsável e autista (n para n)
+CREATE TABLE tbCuidadorAutista (
+    cuidador_id INT NOT NULL,
+    autista_id INT NOT NULL,
+    PRIMARY KEY (cuidador_id, autista_id),
+    FOREIGN KEY (cuidador_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (autista_id) REFERENCES users(id) ON DELETE CASCADE
+)
+-- postagem
+CREATE TABLE tbPostagem (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    tituloPostagem VARCHAR(255)
+    imagemPostagem VARCHAR(255)
+    textoPostagem VARCHAR(1555)
+    data_publicacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+-- mensagem
+CREATE TABLE tbMensagem (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    imagemMensagem VARCHAR(255)
+    textoMensagem VARCHAR(1555)
+    data_publicacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
